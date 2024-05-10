cffc933ddb4d43fb39fdcff1ebae1b7464542985
Leo <leo.q@ardanlabs.com>
2024-05-10 14:19:22 -0700

    fix: nav bar issue

--- forum.js
+++ forum.js
@@ -749,6 +749,7 @@
                     f = t(r),
                     l = document.getElementById(r);
                 if (!l) {
+                    console.log("returning", r, _this);
                     return
                 }
                 var c = n(f),
@@ -864,6 +865,7 @@
             })()
         }("tINaMDSKrgDwAPqTMjNl"),
         function (e) {
+            console.log(t, "pressure and time");
             var n = t("#" + e),
                 r = t("#header03Overlay"),
                 i = t("#menuIcon"),
@@ -1264,7 +1266,7 @@
                         cb: function () {
                             var n = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null,
                                 r = arguments.length > 1 ? arguments[1] : undefined;
-
+                            console.log("cb", r, n);
                             var i = r.form.success;
                             d.trigger("convertly_form_submission_post_request", {
                                 formName: h,
