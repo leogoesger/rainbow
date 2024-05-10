84b798016a36aba036e9499709b193ce7dce4b77
Leo <leo.q@ardanlabs.com>
2024-05-10 11:04:10 -0700

    feat: init commit

---
+++ dynamic-phone.js
@@ -0,0 +1,18 @@
+// Get the value of the "source" parameter from the URL
+const urlParams = new URLSearchParams(window.location.search);
+var source = urlParams.get('adsource');
+if (source == null) {
+    source = localStorage.getItem('adsource');
+    console.log(source)
+    window.location.replace(window.location.href + "?adsource=" + source);
+}
+if (source !== null && source !== "") {
+    localStorage.setItem('adsource', source);
+    if (source.toLowerCase() === 'google') {
+        $("a.dynamicPhoneNumber").attr("href", "tel:4087818428");
+        $("div.dynamicPhoneText").html("TEXT or CALL: (408) 781-8428");
+    } else if (source.toLowerCase() === 'facebook') {
+        $("a.dynamicPhoneNumber").attr("href", "tel:4087818888");
+        $("div.dynamicPhoneText").html("TEXT or CALL: (408) 781-8888");
+    }
+}
